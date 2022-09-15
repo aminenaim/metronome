e4430fdbfd3fce70a6c339254811f41af28a275f
Jhiliano <ggfourcroy@gmail.com>
2022-09-16 00:20:13 +0200

    geometry + image + week firsts classes

---
+++ src/lib/geometry.py
@@ -0,0 +1,80 @@
+from array import ArrayType
+from enum import Enum
+
+class Axe(Enum):
+    ABSCISSA = 0
+    ORDINATE = 1
+
+class Point:
+    def __init__(self, x: float, y: float) -> None:
+        self.x = x
+        self.y = y
+    
+    def __add__(self,p: 'Point') -> 'Point':
+        return Point(self.x + p.x, self.y + p.y)
+    
+    def __sub__(self,p: 'Point') -> 'Point':
+        return Point(self.x - p.x, self.y - p.y)
+
+class Area:
+    def __init__(self, p1: Point, p2: Point, content=None) -> None:
+        self.p1 = p1
+        self.p2 = p2
+        self.content = content
+    def __init__(self, x : float, y : float, w: float, h: float, content=None) -> None:
+        self.p1 = Point(x,y)
+        self.p2 = Point(x+w,y+h)
+        self.content = content
+    
+    def x1(self) -> float:
+        return self.p1.x
+    
+    def x2(self) -> float:
+        return self.p2.x
+    
+    def y1(self) -> float:
+        return self.p1.y
+    
+    def y2(self) -> float:
+        return self.p2.y
+    
+    def w(self) -> float:
+        return abs(self.p2.x - self.p1.x)
+    
+    def h(self) -> float:
+        return abs(self.p2.y - self.p1.y)
+    
+    def in_bound(self, area: 'Area', uncertainty: int = 0) -> bool:
+        return  (self.x1 <= area.x1 + uncertainty) and \
+                (self.x2 >= area.x2 - uncertainty) and \
+                (self.y1 <= area.y1 + uncertainty) and \
+                (self.y2 >= area.y2 - uncertainty)
+    
+    def slice(self, nb_slices: int,  axe: Axe) -> ArrayType:
+        slices = []
+        if(axe == Axe.ABSCISSA):
+            for i in range (0,nb_slices):
+                slices.append(Area(self.x1  + i * self.w//nb_slices, self.y1, self.w//nb_slices, self.h))
+        else:
+            for i in range (0,nb_slices):
+                slices.append(Area(self.x1, self.y1 + i * self.h//nb_slices, self.w, self.h//nb_slices))
+        return slices
+
+class Range:
+    def __init__(self, a: float, b: float, type: int) -> None:
+        assert type == Axe.ABSCISSA or type == Axe.ORDINATE, f"You must pass the type Range.ABSCISSA ({Axe.ABSCISSA}) or Range.ORDONATE ({Axe.ORDINATE})"
+        self.a = a
+        self.b = b
+        self.type = type
+    
+    def middle(self) -> float:
+        return (self.b + self.a)/2
+    
+    def in_range(self, area: Area) -> bool:
+        if self.type == Axe.ABSCISSA:
+            return area.x1 < self.middle < area.x2
+        else:
+            return area.y1 < self.middle < area.y2
+    
+    def between(self, value: float) -> bool:
+        return self.a <= value and value <= self.b
---
+++ src/lib/image.py
@@ -0,0 +1,47 @@
+from array import ArrayType
+import cv2
+import numpy as np
+from lib.geometry import Axe, Area, Point, Range
+
+class Image:
+    def __init__(self, path: str, margin: Area = Area(Point(0,0),Point(0,0))):
+        self.color = cv2.imread(path, cv2.IMREAD_COLOR)
+        h, w = self.color.shape
+        self.color = self.color[margin.y1:h - margin.y2, margin.x1:w - margin.x2]
+        self.gray = cv2.cvtColor(self.color, cv2.COLOR_BGR2GRAY)
+        self.area = Area(margin.p1, Point(w - margin.x2,h - margin.y2))
+    
+    def __init__(self, image: 'Image',  margin: Area = Area(Point(0,0),Point(0,0))):
+        self.color = image.color[margin.y1:image.area.h - margin.y2, margin.x1:image.area.w - margin.x2]
+        self.gray = image.color[margin.y1:image.area.h - margin.y2, margin.x1:image.area.w - margin.x2]
+        self.area = Area(image.area.p1 + margin.p1, Point(image.area.x2 - margin.x2, image.area.y2 - margin.y2))
+        
+    def find_contours(self, dilate: bool = False, all: bool = True, width: Range = None, avg_height: int = None) -> ArrayType:
+        _, modified_image = cv2.threshold(self.gray, 130, 255, cv2.THRESH_BINARY_INV)
+        mode = cv2.RETR_TREE if all else cv2.RETR_EXTERNAL
+        if dilate:
+            kernel = np.ones((4,4), np.uint8)
+            modified_image = cv2.dilate(modified_image, kernel, iterations = 1)
+        contours, _ = cv2.findContours(modified_image, mode, cv2.CHAIN_APPROX_SIMPLE)
+        coordinates = []
+        for contour in contours:
+            area = Area(cv2.boundingRect(contour))
+
+            if width is None or width.between(area.w):
+                if avg_height is None:
+                    coordinates.append(area)
+                else:
+                    nb_slices = round(area.h/avg_height)
+                    coordinates = coordinates + area.slice(nb_slices, Axe.ORDINATE)
+        return coordinates
+    
+    def sub(self, margin: Area = Area(Point(0,0),Point(0,0))) -> 'Image':
+        return Image(self,margin)
+    
+    def show(self, title = "", color: bool = True):
+        cv2.imshow(title,self.color if color else self.gray)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    
+    def save(self, path: str, name: str, color: bool = True):
+        cv2.imwrite(f"{path}/{name}.jpg", self.color if color else self.gray)
