20961a671805e76b0641b51296e878c2b8a1c157
Jhiliano <ggfourcroy32@outlook.fr>
2022-10-27 18:55:16 +0200

    up version minor

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Jackiebot
 
-Version 1.2.1
+Version 1.2.3
 
 This script parse STRI edt from pdf to ics format.
 
--- src/parser.py
+++ src/parser.py
@@ -8,7 +8,7 @@ from lib.document import Metadata, Page, Pdf
 from lib.schedule import Group, Week, Course
 from ics import Calendar, Event
 
-VERSION = '1.2.2'
+VERSION = '1.2.3'
 HELP = ("This script parse the well formed and very useful ( :D ) STRI pdf\n"
         "\n"
         "Options:\n"
