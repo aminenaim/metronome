349a2d43e169c62580fe41c8afea6b30014bb9d4
Jhiliano <ggfourcroy32@outlook.fr>
2022-11-04 16:15:49 +0100

    Refactoring and commenting AreaList

--- src/geometry/arealist.py
+++ src/geometry/arealist.py
@@ -7,16 +7,40 @@ from .point import Point
 
 
 class AreaList(List[Area]):
+    """Class inherited from list and storing area with utils
+    """
+    
     def __init__(self) -> None:
+        """Constructor of an AreaList from the list constructor
+        """
         super().__init__()
     
     def last(self) -> Area:
+        """Get last area from this list
+
+        Returns:
+            Area: the last area
+        """
         return self[len(self) - 1]
 
     def first(self) -> Area:
+        """Get first area from this list
+
+        Returns:
+            Area: the first area
+        """
         return self[0]
     
     def contained(self, area: Area, remove: bool = False) -> 'AreaList':
+        """Get areas contained by the given area
+
+        Args:
+            area (Area): area reference
+            remove (bool, optional): if set to True, delete from the list any found area. Defaults to False.
+
+        Returns:
+            AreaList: list of found area
+        """
         sub = AreaList()
         i = 0
         while(i < len(self)):
@@ -28,7 +52,18 @@ class AreaList(List[Area]):
             i+=1
         return sub
 
-    def match(self, pattern:  Pattern, remove: bool = False) -> 'AreaList':
+    def match(self, pattern: Pattern, remove: bool = False) -> 'AreaList':
+        """Get areas with content matching this pattern.
+        
+        Area content should be string
+
+        Args:
+            pattern (Pattern): regex pattern
+            remove (bool, optional): if set to True, delete from the list any found area. Defaults to False.
+
+        Returns:
+            AreaList: list of found area
+        """
         sub = AreaList()
         i = 0
         while(i < len(self)):
@@ -41,5 +76,10 @@ class AreaList(List[Area]):
         return sub
     
     def change_origin(self, point: Point) -> None:
+        """Change the origin point of each area
+
+        Args:
+            point (Point): origin point
+        """
         for w in self:
             w.change_origin(point)
