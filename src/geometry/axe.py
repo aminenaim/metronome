bc99812abcef82bf89122d26d6a6b12b14a5ef01
Jhiliano <ggfourcroy32@outlook.fr>
2022-11-04 16:30:08 +0100

    Refactoring and commenting Axe

--- src/geometry/axe.py
+++ src/geometry/axe.py
@@ -1,13 +1,31 @@
-from typing import Any
+from typing import Any, Dict
 
 
-class Axe(dict):
+class Axe(Dict[int, Any]):
+    """Class inherited from dict defining a one dimention axe with element in it
+    """
     def __init__(self) -> None:
+        """Constructor of and Axe from the dict constructor
+        """
         super().__init__()
     
     def add(self, x : int, value) -> None:
+        """Add coordinate with value to the dict
+
+        Args:
+            x (int): _description_
+            value (_type_): _description_
+        """
         self[x] = value
     
     def closest(self, x: int) -> Any:
+        """Get the closest element form the Axe
+
+        Args:
+            x (int): coordinate reference
+
+        Returns:
+            Any: content of the closest element from the reference
+        """
         key = min(self, key=lambda l:abs(l-x))
         return self[key]
