e4df8c4bf8c2d6c13033745541e48553145eb333
Jhiliano <ggfourcroy32@outlook.fr>
2022-11-04 16:47:01 +0100

    AxeType comment

--- src/geometry/axetype.py
+++ src/geometry/axetype.py
@@ -2,5 +2,14 @@ from enum import Enum
 
 
 class AxeType(Enum):
+    """Axe type Enumaration
+
+    Args:
+        Enum (_type_): _description_
+    """
     ABSCISSA = 0
+    """Abscissa axe (x coordinates)
+    """
     ORDINATE = 1
+    """Ordinate axe (x coordinates)
+    """
