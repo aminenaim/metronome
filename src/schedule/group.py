92691eec28c082f090b405f54968ccfb52e87fe2
Jhiliano <ggfourcroy32@outlook.fr>
2022-11-11 01:26:21 +0100

    comment on group

--- src/schedule/group.py
+++ src/schedule/group.py
@@ -2,10 +2,23 @@ from enum import Enum
 
 
 class Group(Enum):
+    """Group enumeration
+    """
     ALL = 0
+    """Whole class
+    """
     GROUP1 = 1
+    """First group
+    """
     GROUP2 = 2
+    """Second group
+    """
     
     def __str__(self) -> str:
+        """String formating of group
+
+        Returns:
+            str: formated group
+        """
         value = {Group.ALL: 'Classe Entière', Group.GROUP1: 'Groupe 1', Group.GROUP2: 'Groupe 2'}
         return value[self]
